395cd5e5253d97e445376aadd3448d33ead0e61a
Dustin Fedie <dlfedie@gmail.com>
2019-07-15 11:16:37 -0500

    Started!

--- client.js
+++ client.js
@@ -1,6 +1,32 @@
-console.log('js');
 $(document).ready(readyNow);
 
+const budget = 25000;
+
+let purchases = [];
+
+
 function readyNow() {
-  console.log('jQ');
-}
+  //display budget
+  //target budgetOut by ID
+  let el = $('#budgetOut');
+  el.empty();
+  el.append(budget);
+  //handle click event
+  $('#addPurchaseButton').on('click', addPurchase)
+
+} //end readyNow
+
+function addPurchase() {
+  console.log('in addPurchase');
+  //get user input create new object
+  let newPurchase = {
+    name: $('#purchaseNameIn').val(),
+    price: $('#purchasePriceIn').val()
+  } //end newPurchase
+  //push purchase into array
+  purchases.push(newPurchase);
+  //empty inputs
+  $('#purchaseNameIn').val('');
+  $('#purchasePriceIn').val('');
+  //calculate remainingBudget
+} //end addPurchase
