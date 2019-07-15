6b43ef03d8d6b15d70a26a2be941fb8f0cf07ede
Dustin Fedie <dlfedie@gmail.com>
2019-07-15 14:05:53 -0500

    Bwahahaha jQuery is sweet, you guys

--- client.js
+++ client.js
@@ -5,16 +5,7 @@ const budget = 25000;
 let purchases = [];
 
 
-function readyNow() {
-  //display budget
-  //target budgetOut by ID
-  let el = $('#budgetOut');
-  el.empty();
-  el.append(budget);
-  //handle click event
-  $('#addPurchaseButton').on('click', addPurchase)
 
-} //end readyNow
 
 function addPurchase() {
   console.log('in addPurchase');
@@ -29,4 +20,51 @@ function addPurchase() {
   $('#purchaseNameIn').val('');
   $('#purchasePriceIn').val('');
   //calculate remainingBudget
+  calculateRemainingBudget();
+  //update DOM
+  displayPurchases();
 } //end addPurchase
+
+function calculateRemainingBudget() {
+  console.log('in calculateRemainingBudget');
+  //loop through purchases array
+  let totalPrices = 0;
+  for (let i = 0; i < purchases.length; i++) {
+    //for each purchase, add prices together
+    totalPrices += Number(purchases[i].price);
+    console.log('inside loop:', totalPrices);
+  } // end for
+  console.log('totalPrices:', totalPrices);
+  //subtract total prices from budget for remainingBudget
+  const remainingBudget = budget - totalPrices;
+  //display remainingBudget
+  let el = $('#remainingBudgetOut');
+  el.empty();
+  el.append(remainingBudget);
+} //end calculateRemainingBudget
+
+function displayPurchases() {
+  console.log('in displayPurchases');
+  //target output by ID
+  let el = $('#purchasesOut');
+  //empty
+  el.empty();
+  //loop through purchases array
+  for (purchase of purchases) {
+    //for each purchase, create a list item
+    el.append(`<li>` + purchase.name + `: $` + purchase.price + `</li>`);
+  } // end for
+} //end displayPurchases
+
+
+function readyNow() {
+  //display budget
+  //target budgetOut by ID
+  let el = $('#budgetOut');
+  el.empty();
+  el.append(budget);
+  //handle click event
+  $('#addPurchaseButton').on('click', addPurchase)
+  //init display
+  calculateRemainingBudget();
+} //end readyNow
